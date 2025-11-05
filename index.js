6ea34cb28b558d0b0d03f8672a30cd2484928513
Saugat Rasaili <saugatrasaili76@gmail.com>
2025-11-05 15:30:33 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function checker(a){
+    if(a%2==0){
+        console.log("The number is even");
+
+    }
+
+    else{
+        console.log("The number is odd");
+    }
+    return a;       
+}
+
+ checker(5);
