e9237e9c8fe69d60a258aa253cce2e6b43d1c737
Saugat Rasaili <saugatrasaili76@gmail.com>
2025-11-19 16:27:38 +0545

    updated

---
+++ Even.js
@@ -0,0 +1,9 @@
+
+ function num(a){
+    if(a%2==0){
+        console.log(`The number ${a} is Even`);
+    } else {
+        console.log(`The number ${a} is Odd`);
+    }   
+ }
+    num(7);
---
+++ Natural.js
@@ -0,0 +1,11 @@
+// Sum of Natural Numbers
+ function sum(n) {
+     let sum = 0;
+        for (let i = 0; i <= n; i++) {
+            sum += i;
+        }
+       return sum;
+
+    }
+
+   console.log("The sum of n natural number is",sum(5));
