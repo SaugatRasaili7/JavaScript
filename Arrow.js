0fae5a9181d0d6c949bb63c56480d470efbb200f
Saugat Rasaili <saugatrasaili76@gmail.com>
2025-11-26 15:47:24 +0545

    updated

---
+++ Arrow.js
@@ -0,0 +1,13 @@
+// Arrow Function
+
+   const greater = (a,b) => {
+    if(a>b){
+        console.log(`The number ${a} is greater`);
+    }
+    else{
+        console.log(`The number ${b} is greater`);
+    }
+   } 
+                                       
+   greater(5,9);
+   
